5fe100f14f5a7b9d9f65e277164f396ac6cb4baa
Smok419 <danilmaksimovih@gmail.com>
2024-12-24 19:55:17 +0300

    Repair now cost 0 experience.

--- src/main/java/net/smok/mixin/AnvilHandlerMixin.java
+++ src/main/java/net/smok/mixin/AnvilHandlerMixin.java
@@ -1,7 +1,6 @@
 package net.smok.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -9,18 +8,47 @@ import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.Property;
 import net.smok.Debug;
 import net.smok.koval.items.KovalItem;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AnvilScreenHandler.class)
-public class AnvilHandlerMixin {
+public abstract class AnvilHandlerMixin {
+
+    @Shadow
+    private int repairItemUsage;
+
+    @Shadow
+    public static int getNextCost(int cost) {
+        return 0;
+    }
 
 
+    @Redirect(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;set(I)V", ordinal = 5))
+    private void resetLevelCost(Property levelCost, int defaultCost, @Local(ordinal = 0) ItemStack main, @Local(ordinal = 1) int j, @Local(ordinal = 2) int k) {
+        // if only repair - 0, if with repair - prev repair
 
+        if (main.getItem() instanceof KovalItem && repairItemUsage > 0)
+             levelCost.set(k == 0 ? 0 : j);
+        else levelCost.set(defaultCost);
+    }
 
+    @ModifyConstant(method = "canTakeOutput", constant = @Constant(intValue = 0))
+    private int canTakeOutput(int constant) {
+        // if it is repairing, then we can take output even 0 level cost
+        return repairItemUsage > 0 ? -1 : constant;
+    }
+
+    @Redirect(method = "updateResult", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/screen/AnvilScreenHandler;getNextCost(I)I"
+        )
+    )
+    private int resetNextCost(int cost, @Local(ordinal = 0) ItemStack main) {
+        Debug.log("Item "+main + " usage "+repairItemUsage);
+        if (main.getItem() instanceof KovalItem) return cost;
+        return getNextCost(cost);
+    }
 
     @ModifyArg(method = "updateResult",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z", ordinal = 1)
